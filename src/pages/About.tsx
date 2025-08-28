d396a4ca1b98a89f1a81d3d8ccc893bbbced35f3
midoboukhatem <midoboukhatem@gmail.com>
2025-08-28 14:03:50 +0200

    add scroll option in about page in phone view

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,7 +4,10 @@ import SocialButtons from '@/components/layout/SocialButtons';
 const About = () => {
   return (
     <section id="about" className="about-page min-h-screen py-12 flex flex-col">
-      <div className="section-container flex-grow">
+      <div
+        className="section-container md:overflow-visible md:h-auto overflow-auto overscroll-contain h-[calc(100vh-6rem)]"
+        data-allow-scroll
+      >
         <AboutIntro />
       </div>
       {/* <SocialButtons /> */}
